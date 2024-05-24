53af970f847115fc2c2e3e2e6e7df1819726da40
= <=>
2024-05-24 15:56:04 +0100

    added

--- src/dashboard/Login.js
+++ src/dashboard/Login.js
@@ -25,7 +25,41 @@ const Login = () => {
             const data = {email: email, password: password};
 
             const request = await postData(url, headers, data)
-            if(request.error == ""){
+            //alert(JSON.stringify(request))
+            
+            if(request.error == "" && request.result.data?.error != "error"){
+                if(request.result.data?.error == ""){
+                    
+                    //alert("No error")
+                    localStorage.setItem('_id', request.result.data.result.result._id);
+                    localStorage.setItem('jwt_token', request.result.data.result.token);
+                    localStorage.setItem('email', request.result.data.result.result.email);
+                    localStorage.setItem('name', request.result.data.result.result.name);
+                    window.location.href = `${getSiteBaseURL()}/users`
+                    
+
+
+                }else{
+                    setGenError(request.result.data?.result)
+                }
+
+            }else if(request.result.data?.error == "error"){
+                 //alert(JSON.stringify(request.result.data))
+                 if(request.result.data?.status == 409){
+                    setGenError(request.result.data?.result)
+                }else if(request.result.data?.status == 404){
+                    setGenError(request.result.data?.result)
+                } else{
+                    setGenError("check your form for errors")
+                }
+                
+            }
+                
+            else{
+                setGenError("something went wrong")
+            }
+
+            /*if(request.error == ""){
                 if(request.result.data.error == ""){
                     //alert("Success")
                     //alert(JSON.stringify(request.result.data.result.result))
@@ -42,33 +76,34 @@ const Login = () => {
 
             }else{
                 setGenError("Something went wrong")
-            }
-            setBtnState("Sign in")
-        }  
+            }*/
+            
+        }
+        setBtnState("Sign in")
     }
 
     return(
-        <section class="bg-gray-50 dark:bg-gray-900">
+        <section class="bg-gray-50">
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
                 <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
                 Carpark    
             </a>
-            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                         Sign in to your account
                     </h1>
                     <div class="space-y-4 md:space-y-6">
                         {genError != "" ? <div class="text-red-500 text-sm font-semibold">{genError}</div> : ""}
                         <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required=""/>
                             {error.find(item => item.field == "email").msg ? <p class="text-red-500 text-sm font-semibold">{error.find(item => item.field == "email").msg }</p>: null}
                         </div>
                         <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required=""/>
                             {error.find(item => item.field == "password").msg ? <p class="text-red-500 text-sm font-semibold">{error.find(item => item.field == "password").msg }</p>: null}
                         </div>
                         {/*<div class="flex items-center justify-between">
@@ -83,8 +118,8 @@ const Login = () => {
                             <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                         </div>*/}
                         
-                        <button onClick={() => login()} type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">{btnState}</button>
-                        <a className=" underline text-blue-800" href="/signup">Signup</a>
+                        <button onClick={() => login()} type="submit" class="w-full text-black bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">{btnState}</button>
+                        <a className=" underline text-blue-800 text-lg mt-12" href="/signup">Signup</a>
                         {/*<p class="text-sm font-light text-gray-500 dark:text-gray-400">
                             Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                         </p>*/}
--- src/dashboard/signup.js
+++ src/dashboard/signup.js
@@ -28,10 +28,32 @@ const Signup = () => {
             const data = {name: name, email: email, password: password};
 
             const request = await postData(url, headers, data)
-            if(request.error == ""){
+            //alert(JSON.stringify(request))
+            if(request.error == "" && request.result.data?.error != "error"){
+                if(request.result.data?.error == ""){
+                    
+                    window.location.href = `${getSiteBaseURL()}/login`
+
+                }else{
+                    setGenError(request.result.data?.result)
+                }
+
+            }else if(request.result.data?.error == "error"){
+                
+                //alert(JSON.stringify(request.result.data))
+                if(request.result.data?.status == 409){
+                    setGenError(request.result.data?.result)
+                }else{
+                    setGenError("check your form for errors")
+                }
+            }
+
+
+
+            /*if(request.error == ""){
                 if(request.result.data.error == ""){
-                    //alert("Success")
-                    //alert(JSON.stringify(request.result.data.result.result))
+                    alert("Success")
+                    alert(JSON.stringify(request.result.data.result.result))
                     //localStorage.setItem('_id', request.result.data.result.result._id);
                     //localStorage.setItem('jwt_token', request.result.data.result.token);
                     //localStorage.setItem('email', request.result.data.result.result.email);
@@ -46,38 +68,38 @@ const Signup = () => {
 
             }else{
                 setGenError("Something went wrong")
-            }
+            }*/
             setBtnState("Sign in")
-        }  
+        } 
     }
 
     return(
-        <section class="bg-gray-50 dark:bg-gray-900">
+        <section class="bg-gray-50 ">
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
                 <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
                 Carpark    
             </a>
-            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                         Admin signup
                     </h1>
                     <div class="space-y-4 md:space-y-6">
                         {genError != "" ? <div class="text-red-500 text-sm font-semibold">{genError}</div> : ""}
                         <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-                            <input value={name} onChange={(e) => setName(e.target.value)} type="name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Muhammad Sani" required=""/>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your name</label>
+                            <input value={name} onChange={(e) => setName(e.target.value)} type="name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Muhammad Sani" required=""/>
                             {error.find(item => item.field == "name").msg ? <p class="text-red-500 text-sm font-semibold">{error.find(item => item.field == "name").msg }</p>: null}
                         </div>
                         <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required=""/>
                             {error.find(item => item.field == "email").msg ? <p class="text-red-500 text-sm font-semibold">{error.find(item => item.field == "email").msg }</p>: null}
                         </div>
                         <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required=""/>
                             {error.find(item => item.field == "password").msg ? <p class="text-red-500 text-sm font-semibold">{error.find(item => item.field == "password").msg }</p>: null}
                         </div>
                         {/*<div class="flex items-center justify-between">
@@ -91,8 +113,8 @@ const Signup = () => {
                             </div>
                             <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                         </div>*/}
-                        <button onClick={() => signup()} type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">{btnState}</button>
-                        <a className=" underline text-blue-800" href="/login">Login</a>
+                        <button onClick={() => signup()} type="submit" class="w-full text-black bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">{btnState}</button>
+                        <a className=" underline text-blue-800 tex-lg mt-12" href="/login">Login</a>
                         
                         {/*<p class="text-sm font-light text-gray-500 dark:text-gray-400">
                             Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
