973d9707efa2a22d91e9e541013021a8f809b8c8
= <=>
2024-09-02 06:39:32 +0100

    added

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const allconfig = {
-    env: 'dev',
+    env: 'prod',
     dev_api_url : 'http://localhost:8000',
     dev_site_url: 'http://localhost:3000',
-    prod_api_url : 'https://cryptolits-admin.herokuapp.com',
+    prod_api_url : 'https://studentcomplaint-server.vercel.app',
     prod_site_url: 'http://dashboard-xyz.cryptolits.org',
     prod_landing_url: 'http://cryptolits.com',
 }
