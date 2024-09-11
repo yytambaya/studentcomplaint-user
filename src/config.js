44523356f7725bcb594fce8faa7b9c63f8f2abce
= <=>
2024-09-11 23:30:54 +0100

    added

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const allconfig = {
-    env: 'dev',
+    env: 'prod',
     dev_api_url : 'http://localhost:8000',
     dev_site_url: 'http://localhost:3000',
     prod_api_url : 'https://studentcomplaint-server.vercel.app',
